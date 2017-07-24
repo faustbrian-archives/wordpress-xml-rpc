4547ad9c5b95573171cd0759862c910b3ebe26f5
Brian Faust <faustbrian@users.noreply.github.com>
2017-07-24 09:56:08 +0300

    Apply fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('vendor/autoload.php');
+require_once 'vendor/autoload.php';
 
 $client = new BrianFaust\WordPressXmlRpc\Client('http://wordpress.dev/xmlrpc.php', 'password', 'password');
 
